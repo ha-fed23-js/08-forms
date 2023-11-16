cc1d7b0a70fd51a62975f3c313011d2266c14b82
David Andersson <david.andersson@zocom.se>
2023-11-16 15:23:08 +0100

    User profile

--- profile.js
+++ profile.js
@@ -1,1 +1,40 @@
 // Användarprofil
+const defaultUser = {
+	name: 'Guy Fierri',
+	courses: [
+		{ title: 'HTML', isDone: true },
+		{ title: 'JavaScript', isDone: false },
+		{ title: 'UX', isDone: false },
+		{ title: 'Agila metoder', isDone: false }
+	]
+}
+
+function renderUser(user) {
+	const profileName = document.querySelector('.profile-name')
+	const courseList = document.querySelector('.courses')
+	
+	profileName.innerText = user.name
+	
+	let idCounter = 0
+	user.courses.forEach(course => {
+		const id = 'course-checkbox-' + idCounter
+		idCounter++
+		console.log('Course är: ', course);
+		const p = document.createElement('p')
+		
+		const checkbox = document.createElement('input')
+		checkbox.id = id
+		checkbox.type = 'checkbox'
+		checkbox.checked = course.isDone
+		p.append(checkbox)
+	
+		const label = document.createElement('label')
+		label.htmlFor = id
+		label.innerText = course.title
+		p.append(label)
+	
+		courseList.append(p)
+	})
+}
+
+renderUser(defaultUser)
